c6df2cec47aec4e38ce6edd8843c64dfbed50e5d
Asep Sutisna <sutisnaasep323@gmail.com>
2022-03-02 09:11:24 +0700

    Reactive Programming

---
+++ app/src/main/java/com/dicoding/myreactiveform/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.dicoding.myreactiveform
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Patterns
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.dicoding.myreactiveform.databinding.ActivityMainBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
+import io.reactivex.functions.Function3
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*
+        1. skipInitialValue() untuk menghiraukan input awal. Hal ini bertujuan supaya aplikasi tidak langsung menampilkan eror pada saat pertama kali dijalankan.
+        2. Anda menggunakan operator map dan memeriksa apakah format valid. Jika format tidak valid
+        maka ia akan mengembalikan nilai TRUE. Kemudian saat subscribe, kita memanggil fungsi
+        showEmailExistAlert(it) untuk menampilkan peringatan jika hasilnya TRUE.
+        3. adanya variabel karena Anda memerlukan data stream tersebut untuk menentukan Button aktif atau tidak
+         */
+
+        // it adalah variabel default pada lambda. Anda juga dapat mengubah variabel tersebut dengan cara:
+        /*
+            emailStream.subscribe { isValid ->
+                showEmailExistAlert(isValid)
+            }
+         */
+
+        val emailStream = RxTextView.textChanges(binding.edEmail)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe {
+            showEmailExistAlert(it)
+        }
+
+        val passwordStream = RxTextView.textChanges(binding.edPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe {
+            showPasswordMinimalAlert(it)
+        }
+
+        val passwordConfirmationStream = Observable.merge(
+            RxTextView.textChanges(binding.edPassword)
+                .map { password ->
+                    password.toString() != binding.edConfirmPassword.text.toString()
+                },
+            RxTextView.textChanges(binding.edConfirmPassword)
+                .map { confirmPassword ->
+                    confirmPassword.toString() != binding.edPassword.text.toString()
+                }
+        )
+
+        passwordConfirmationStream.subscribe {
+            showPasswordConfirmationAlert(it)
+        }
+
+        val invalidFieldsStream = Observable.combineLatest(
+            emailStream,
+            passwordStream,
+            passwordConfirmationStream,
+            Function3 { emailInvalid: Boolean, passwordInvalid: Boolean, passwordConfirmationInvalid: Boolean ->
+                !emailInvalid && !passwordInvalid && !passwordConfirmationInvalid
+            })
+        invalidFieldsStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnRegister.isEnabled = true
+                binding.btnRegister.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+            } else {
+                binding.btnRegister.isEnabled = false
+                binding.btnRegister.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+            }
+        }
+    }
+
+
+    private fun showEmailExistAlert(isNotValid: Boolean) {
+        binding.edEmail.error = if (isNotValid) getString(R.string.email_not_valid) else null
+    }
+
+    private fun showPasswordMinimalAlert(isNotValid: Boolean) {
+        binding.edPassword.error = if (isNotValid) getString(R.string.password_not_valid) else null
+    }
+
+    private fun showPasswordConfirmationAlert(isNotValid: Boolean) {
+        binding.edConfirmPassword.error = if (isNotValid) getString(R.string.password_not_same) else null
+    }
+}
